3b3e456fdb35bb47842f5d2adb8540a37a8b1f73
ElRharbiBadr <stardust12003@gmail.com>
2023-11-06 15:31:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import Bb from"./comp" //isem dial file comp 
+import './App.css';
+import Comp3 from "./comp3";
+import Comp2 from './comp2';
+import UseRef from './UseRef';
+import Calcul from './comp';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+{/*   
+    <Comp2/> */}
+    <Comp2/>
+ 
+    
+    </div>
+  );
+}
+
+export default App;
